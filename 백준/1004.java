6faa7f95073d59e7c14bfcc7a1c3d6d68b59d8ee
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-04-14 21:00:09 +0900

    Update 1004.java

--- 백준/1004.java
+++ 백준/1004.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+         
         int T = sc.nextInt();
         
         for(int i = 0; i < T; i++){
