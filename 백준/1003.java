cdee2bbd695d66b5b599af825289c14aa92a1785
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-04-15 20:08:20 +0900

    Update 1003.java

--- 백준/1003.java
+++ 백준/1003.java
@@ -5,7 +5,7 @@ class Main{
          Scanner sc = new Scanner(System.in);
          int T = sc.nextInt();
          int[] dp = new int[41];
-         dp[0] = 0;
+         dp[0] = 0; 
          dp[1] = 1;
          dp[2] = 1;
          for(int i = 3; i < 41; i++){
